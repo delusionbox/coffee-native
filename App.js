f39769589a810d74ed5a601e809f62797806ce96
Docofarm <changhyeonseo283@gmail.com>
2022-11-28 17:31:03 +0900

    native start

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { Asset } from 'expo-asset';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+    const fontToLoad = [Ionicons.font]
+    const fontPromises = fontToLoad.map(font => Font.loadAsync(font));
+    const imageToLoad = [require("./assets/logo.jpg"),];
+    const imagePromises = imageToLoad.map(image => Asset.loadAsync(image));
+    return Promise.all([...fontPromises, ...imagePromises]);
+  }
+  if (loading) {
+    return <AppLoading startAsync={preload} onError={console.warn} onFinish={onFinish} />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>HI</Text>
       <StatusBar style="auto" />
     </View>
   );
